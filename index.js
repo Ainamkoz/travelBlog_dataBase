2a9cbee8b92ea02a75bab327164c977a5bb98aad
Jhautier <jhautier@hotmail.fr>
2021-03-24 21:42:00 +0100

    add comments

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const {getAllSpots} = require('./controllers/spots')
 const {getAllRecipes} = require('./controllers/recipes')
 
 const port = process.env.PORT || 3001;//*  recommended by Jorge. This way, the connection to the server will work also when the website is deployed online.
+
 const cors = require('cors') //* CORS aims to avoid errors related to Cross-origin resource sharing
-const CORSOption = {origin: process.env.ORIGIN || 'http://localhost:3000'} 
-app.use(cors(CORSOption)) //*  allows our react application (by default set on localhost:3000) to access the server (by default set on localhost:3001)
+const CORSOption = {origin: process.env.ORIGIN || 'http://localhost:3000'} //* this line refers to our FRONT-END server
+
+app.use(cors(CORSOption)) //*  this line allows the FRONT-END(port 3000) to take information from the back-end(port 3001)
 app.use(express.json());
 
 
